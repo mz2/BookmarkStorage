4ed3a0b1729acde6f628a51ab95becbbd205e2fa
Matias Piipari <matias.piipari@gmail.com>
2016-09-04 19:44:51 +0100

    Initial commit.

---
+++ BookmarkStorage/BookmarkStorage.h
@@ -0,0 +1,19 @@
+//
+//  BookmarkStorage.h
+//  BookmarkStorage
+//
+//  Created by Matias Piipari on 04/09/2016.
+//  Copyright © 2016 Matias Piipari & Co. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BookmarkStorage.
+FOUNDATION_EXPORT double BookmarkStorageVersionNumber;
+
+//! Project version string for BookmarkStorage.
+FOUNDATION_EXPORT const unsigned char BookmarkStorageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BookmarkStorage/PublicHeader.h>
+
+
